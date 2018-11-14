f00913d748a3dc4678aa934e4db07b9c9ab649ab
Lakshmi Prasanthi <37037792+prasanthi21@users.noreply.github.com>
2018-11-14 12:44:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -154,50 +154,9 @@ class App extends Component {
         this.setState({
             'prevmarker': marker
         });
-        this.state.infowindow.setContent('Loading Data...');
         this.state.map.setCenter(marker.getPosition());
         this.state.map.panBy(0, -200);
-        this.getMarkerInfo(marker);
     }
-
-    /**
-     * Retrive the location data from the foursquare api for the marker and display it in the infowindow
-     * @param {object} location marker
-     */
-    getMarkerInfo(marker) {
-        var self = this;
-        var clientId = "TPIDDHBKB2QFBWEV2MPDOFGUSWXCXGAA5IVOWEMN5ASR3UJW";
-        var clientSecret = "4HB1ZZJBVXC3F0BREBPSGXYK0VZ5ALS4XRNJZSBP1JROG0DE";
-        var url = "https://api.foursquare.com/v2/venues/search?client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20130815&ll=" + marker.getPosition().lat() + "," + marker.getPosition().lng() + "&limit=1";
-        fetch(url)
-            .then(
-                function (response) {
-                    if (response.status !== 200) {
-                        self.state.infowindow.setContent("Sorry data can't be loaded");
-                        return;
-                    }
-
-                    // Examine the text in the response
-                    response.json().then(function (data) {
-                        var location_data = data.response.venues[0];
-                        var verified = '<b>Verified Location: </b>' + (location_data.verified ? 'Yes' : 'No') + '<br>';
-                        var checkinsCount = '<b>Number of CheckIn: </b>' + location_data.stats.checkinsCount + '<br>';
-                        var usersCount = '<b>Number of Users: </b>' + location_data.stats.usersCount + '<br>';
-                        var tipCount = '<b>Number of Tips: </b>' + location_data.stats.tipCount + '<br>';
-                        var readMore = '<a href="https://foursquare.com/v/'+ location_data.id +'" target="_blank">Read More on Foursquare Website</a>'
-                        self.state.infowindow.setContent(checkinsCount + usersCount + tipCount + verified + readMore);
-                    });
-                }
-            )
-            .catch(function (err) {
-                self.state.infowindow.setContent("Sorry data can't be loaded");
-            });
-    }
-
-    /**
-     * Close the infowindow for the marker
-     * @param {object} location marker
-     */
     closeInfoWindow() {
         if (this.state.prevmarker) {
             this.state.prevmarker.setAnimation(null);
